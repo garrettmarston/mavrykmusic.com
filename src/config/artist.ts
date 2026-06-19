8c9317ecad5f9c81957aeca5e1d324a0ee89d2c3
garrettmarston <marstongarrett@gmail.com>
2026-06-18 23:48:50 -0700

    Update artist.ts

--- src/config/artist.ts
+++ src/config/artist.ts
@@ -187,7 +187,7 @@ export const latestRelease: LatestReleaseConfig = {
   image: "/images/Album Cover Art SIZELAND.png",
   imageAlt: "Messed Up",
   streamingLinks: [
-    { label: "Listen Now!", url: "" },
+    { label: "Listen Now!", url: "https://open.spotify.com/track/40hohBelNmsgAZyY1NACBy?si=bdaecfb1dc33447a" },
   ],
 
   backgroundColorOverride: null,
